32a35c503b416709398e08224cfb7fe31d567710
nk <nk@192.168.0.21>
2018-05-04 00:54:53 -0700

    Test 222 Alpha Beta
    Change-Id: Ib1395bba9dabea7051c7ccd1756bac65623e7df9
    Signed-off-by: nk <nk@192.168.0.21>

--- spark/src/main/scala/org/test/WordCount.scala
+++ spark/src/main/scala/org/test/WordCount.scala
@@ -6,7 +6,8 @@ import org.apache.spark.SparkContext
 object WordCount {
 
   def main(args: Array[String]) = {
-
+    //Test
+    //Test latest alpha
     //Start the Spark context
     val conf = new SparkConf()
       .setAppName("WordCount")
@@ -14,7 +15,7 @@ object WordCount {
     val sc = new SparkContext(conf)
 
     //Read some example file to a test RDD
-    val test = sc.textFile("food.txt")
+    val test = sc.textFile(args(0))
 
     test.flatMap { line => //for each line
       line.split(" ") //split the line in word by word. NB: we use flatMap because we return a list
@@ -23,7 +24,7 @@ object WordCount {
       (word,1) //return a key/value tuple, with the word as key and 1 as value
     }
     .reduceByKey(_ + _) //sum all of the value with same key
-    .saveAsTextFile("food.counts.txt") //save to a text file
+    .saveAsTextFile(args(1)) //save to a text file
 
     //Stop the Spark context
     sc.stop
